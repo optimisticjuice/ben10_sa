f5fed407413d59b3e8a134bd395fcaf1e45221cc
Optimistic Juice <justamanuelax@gmail.com>
2026-02-23 13:32:34 +0200

    Still trying to fix the layout

--- src/ClassicBen10/ben10.tsx
+++ src/ClassicBen10/ben10.tsx
@@ -1,7 +1,21 @@
 import "./ben10.css"
 function Ben10(){
     return(
+        <>
         <h1 className="ben10-title">Ben 10</h1>
+        <div className="buttons">
+        <button className="ben10-button">Play</button>
+
+        <button className="ben10-button">Play</button>
+
+        <button className="ben10-button">Play</button>
+
+        <button className="ben10-button">Play</button>
+
+        <button className="ben10-button">Play</button>
+
+        </div>
+        </>
     )
 }
 export default Ben10;
